551fd10b90ead338c71ba4ee9a2f20377e849d1b
José Brito <josealbritose@gmail.com>
2024-04-02 20:28:09 -0300

    FetchData

--- src/services/CryptoService.ts
+++ src/services/CryptoService.ts
@@ -1,11 +1,22 @@
 import axios from "axios";
-import { CryptoCurrenciesResponseSchema } from "../schema/cryptoSchema";
+import { CryptoCurrenciesResponseSchema, CryptoPriceSchema } from "../schema/cryptoSchema";
+import { Pair } from "../types";
 
 export async function getCryptos(){
   const limit = 20;
   const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=${limit}&tsym=USD`
   const { data: { Data } } = await axios.get(url);
   const result = CryptoCurrenciesResponseSchema.safeParse(Data);
+  if(result.success){
+    return result.data
+  }
+}
+
+export async function fetchCurrencyCryptosPrice({currency,cryptocurrency}:Pair){
+  const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptocurrency}&tsyms=${currency}`;
+
+  const { data: { DISPLAY } } = await axios.get(url);
+  const result = CryptoPriceSchema.safeParse(DISPLAY[cryptocurrency][currency])
   if(result.success){
     return result.data
   }
