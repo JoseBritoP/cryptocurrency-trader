d37acbb232d93613dc03cc3a2e28f006f10e6b68
José Brito <josealbritose@gmail.com>
2024-04-02 20:05:53 -0300

    Pair type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,7 @@
 import z from 'zod'
-import { CurrencySchema,CryptoCurrencyResponseSchema } from '../schema/cryptoSchema'
+import { CurrencySchema,CryptoCurrencyResponseSchema, PairSchema } from '../schema/cryptoSchema'
 
 export type Currency = z.infer<typeof CurrencySchema>;
 
 export type CryptoCurrency = z.infer<typeof CryptoCurrencyResponseSchema>
+export type Pair = z.infer<typeof PairSchema>
