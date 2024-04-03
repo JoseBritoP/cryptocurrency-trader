330d012e4e76b205b91dbbdd9fb5da2495bc4ed9
José Brito <josealbritose@gmail.com>
2024-04-03 00:22:04 -0300

    Styles

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,11 @@ function App() {
   },[]);
   
   return (
-    <main className='max-w-[60rem] w-[95%] my-0 mx-auto text-white'>
-      <section className='my-0 mx-auto'>
-        <h1 className='mt-20 font-bold text-6xl text-center'>Cryptocurrency <span className='block text-primary'>Quote</span></h1>
+    <main className='max-w-[60rem] w-[95%] mx-auto text-white'>
+      <section className='my-0 mx-auto pt-20'>
+        <h1 className='font-bold text-6xl text-center'>Cryptocurrency <span className='block text-primary'>Quote</span></h1>
       </section>
-      <section className='flex flex-col items-center gap-y-10 mt-20 py-24 px-8 bg-slate-950 border-2 rounded-md shadow-md'>
+      <section className='flex flex-col items-center gap-y-4 mt-20 py-10 px-5 bg-slate-950 border-2 rounded-md shadow-md'>
         <CryptoSearchForm/>
         <CryptoPriceDisplay/>
       </section>
--- src/components/CryptoPriceDisplay.tsx
+++ src/components/CryptoPriceDisplay.tsx
@@ -6,19 +6,19 @@ export default function CryptoPriceDisplay() {
   const { result } = useCryptoStore();
 
   const hasResult = useMemo(()=> !Object.values(result).includes('') ,[result]);
-  console.log(hasResult)
+
   return (
-    <div className="mt-5 ">
+    <div className="mt-5 flex flex-col gap-y-4 ">
       { hasResult &&  
         (<>
-          <h2 className="text-center text-2xl">Cotitzación</h2>
-          <div className="grid grid-cols-2 items-center gap-4">
-            <img alt={`Image Crypto`}  src={`https://cryptocompare.com/${result.IMAGEURL}`} className="w-full" />
-            <div>
-              <p className="mr-1 font-medium" >El precio es de: <span className="font-semibold" >{result.PRICE}</span></p>
-              <p className="mr-1 font-medium" >El precio más alto es de: <span className="font-semibold" >{result.HIGHDAY}</span></p>
-              <p className="mr-1 font-medium" >El precio más bajo es de: <span className="font-semibold" >{result.LOWDAY}</span></p>
-              <p className="mr-1 font-medium" >Su variación es de: <span className="font-semibold" >{result.CHANGEPCT24HOUR}%</span></p>
+          <h2 className="text-center text-3xl">Quotation</h2>
+          <div className="grid grid-cols-2 items-center gap-20">
+            <img alt={`Image Crypto`}  src={`https://cryptocompare.com/${result.IMAGEURL}`} className="h-48 w-40" />
+            <div className="flex flex-col gap-y-2">
+              <p className="mr-1 font-medium" >Price: <span className="font-semibold" >{result.PRICE}</span></p>
+              <p className="mr-1 font-medium" >High Price: <span className="font-semibold" >{result.HIGHDAY}</span></p>
+              <p className="mr-1 font-medium" >Low Price: <span className="font-semibold" >{result.LOWDAY}</span></p>
+              <p className="mr-1 font-medium" >Variation: <span className="font-semibold" >{result.CHANGEPCT24HOUR}%</span></p>
               <p className="mr-1 font-medium" >Last update: <span className="font-semibold" >{result.LASTUPDATE}</span></p>
             </div>
           </div>
