ccdfbadc2fdec0b7b849164b0e99ae65ae148274
José Brito <josealbritose@gmail.com>
2024-05-10 09:27:54 -0300

    SEO

--- src/components/CryptoSearchForm.tsx
+++ src/components/CryptoSearchForm.tsx
@@ -43,7 +43,7 @@ export default function CryptoSearchForm() {
         {error !== '' && <Alert message={error}/>}
         <div className="flex flex-col gap-5 justify-evenly">
           <label htmlFor="currency" className="font-semibold text-xl text-center">Currency: </label>
-          <select name="currency" id="currency" value={pair.currency} onChange={handleChange} className="py-1 px-2 text-gray-700 rounded-md">
+          <select name="currency" id="currency" value={pair.currency} onChange={handleChange} className="py-2 px-4 text-gray-700 rounded-md">
             <option value="---"> -- Select the currency --</option>
             {currencies.map(({code,name})=>(
               <option key={code} value={code}>{name}</option>
@@ -52,14 +52,14 @@ export default function CryptoSearchForm() {
         </div>
         <div className="flex flex-col gap-5 justify-evenly">
           <label htmlFor="cryptocurrency" className="font-semibold text-xl text-center">Crypto Currency: </label>
-          <select name="cryptocurrency" id="cryptocurrency" value={pair.cryptocurrency} onChange={handleChange} className="py-1 px-2 text-gray-700 rounded-md">
+          <select name="cryptocurrency" id="cryptocurrency" value={pair.cryptocurrency} onChange={handleChange} className="py-2 px-4 text-gray-700 rounded-md">
             <option value="---"> -- Select the currency --</option>
             {cryptoCurrencies.map(({CoinInfo:{FullName,Name}})=>(
               <option key={Name} value={Name}>{FullName}</option>
             ))}
           </select>
         </div>
-        <button aria-label="Check currency" className="hover:bg-[#123866] bg-[#1458aa] transition-colors 300 ease-in-out py-2 px-4 rounded-md text-xl font-semibold border-[1px] border-gray-500 uppercase mt-2">Check currency</button>
+        <button aria-label="Check currency" className="hover:bg-[#123866] bg-[#1458aa] transition-colors 300 ease-in-out py-2 px-4 rounded-md text-xl border-[1px] border-sky-600 uppercase mt-2 font-bold">Check currency</button>
       </legend>
     </form>
   )
